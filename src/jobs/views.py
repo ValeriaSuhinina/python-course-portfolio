9f0f2dbe0a99f441f472a1314b627e8a671cb99a
Валерия Сухинина <vasukhinina@edu.hse.ru>
2024-03-28 10:14:19 +0500

    Добавила поле для подробного описания работы, отредактировала шаблон для отображения работы в списке работ, добавила страничку для подробного просмотра информации о работе (задания 1, 2,3)

--- src/jobs/views.py
+++ src/jobs/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from jobs.models import Job
 
 
 class IndexJobsListView(ListView):
+    model = Job
+
+class JobDetailView(DetailView):
     model = Job
